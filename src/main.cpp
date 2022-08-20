9d05a542c40fbe0eec7fee37a6bfaf47bcd16afc
Adam Ratajczak <a3.ratajczak@gmail.com>
2022-08-20 16:42:31 +0200

    Initial setup

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+int main(){
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Chess 2d");
+
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+
+        window.display();
+    }
+}
